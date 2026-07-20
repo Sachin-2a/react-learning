953de83a205cd52207c3fbf1e19d3d69c7a9d13f
Sachin-2a <sachinacharya2380@gmail.com>
2026-07-20 10:08:47 +0530

    Add App component with greeting and Card2

---
+++ day2/App.jsx
@@ -0,0 +1,16 @@
+
+import Card2 from "./components/card";
+
+function App() {
+  return (
+    <>
+
+ <p className="bg-gray-200 text-black p-4 rounded-2xl inline-block ">
+    Hello world!
+  </p>
+    <Card2 username="jhon doe" btnText="click me"/>
+    </>
+  )
+}
+
+export default App;
