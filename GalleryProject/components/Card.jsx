d25b1bed5acf9c11a5e38a1b6960023e1009f7e9
Sachin-2a <sachinacharya2380@gmail.com>
2026-08-07 08:48:53 +0530

    Create Card.jsx

---
+++ GalleryProject/components/Card.jsx
@@ -0,0 +1,13 @@
+function Card(props){
+    return(
+             <div key={props.idx}>
+          <a href={props.elem.url} target='_blank'>
+      <div className='h-40 w-44 overflow-hidden bg-white rounded-xl' >
+      <img className='h-full w-full object-cover' src={props.elem.download_url}/>
+      </div>
+          <h2 className='font-bold text-lg'>{props.elem.author}</h2>
+          </a>
+      </div>
+    )
+}
+export default Card
