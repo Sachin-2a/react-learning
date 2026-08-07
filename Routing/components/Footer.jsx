c86a62189c2cda211ffc2133dc2f260884203940
Sachin-2a <sachinacharya2380@gmail.com>
2026-08-07 15:26:59 +0530

    Create Footer.jsx

---
+++ Routing/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer=()=>{
+    return (
+        <>
+                <div className="flex justify-center text-2xl py-4 bg-cyan-900   items-center absolute bottom-0 w-full">
+                    <h1>Footer</h1>
+                </div>
+        
+        </>
+    )
+}
+
+export default Footer
